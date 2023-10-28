b4b5b836ffa0e0adfeccdf573d8db5b19a7e2973
omes773 <olockhart88@gmail.com>
2023-10-27 22:55:35 -0500

    fixed error

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -61,7 +61,7 @@ router.put('/:id', async (req, res) => {
 router.delete('/:id', async (req, res) => {
   // delete a category by its id value
   try {
-    const categoryData = await Tag.destroy({
+    const categoryData = await Category.destroy({
       where: {
         id: req.params.id
       }
