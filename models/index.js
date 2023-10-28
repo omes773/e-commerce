f428cfc4d9d6359200d96609f99f26b9e315560b
omes773 <olockhart88@gmail.com>
2023-10-27 21:49:13 -0500

    completed route folders

--- models/index.js
+++ models/index.js
@@ -7,21 +7,25 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: 'category_id'
-})
+});
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: 'category_id'
-})
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
+  as: 'productTag_products',
   foreignKey: 'product_id'
-} )
+});
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
+  as: 'productTag_products',
   foreignKey: 'tag_id'
 })
+
 module.exports = {
   Product,
   Category,
